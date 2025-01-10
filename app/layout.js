64ef6209459871a5cd3d7ef65845014cc3a51680
Chris Rufus Sweeney <rufussweeney@gmail.com>
2025-01-09 17:20:23 -0700

    Add root layout file

---
+++ app/layout.js
@@ -0,0 +1,35 @@
+export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    );
+  }
+
+  export const metadata = {
+    title: 'Your App Name',
+    description: 'Your app description',
+  }
+  
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    );
+  }
+
+  import './global.css'
+
+export const metadata = {
+  title: 'Your App Name',
+  description: 'Your app description',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
