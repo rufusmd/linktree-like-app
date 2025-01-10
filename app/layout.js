c0e4a8de7867d375ab37eb90e6f3aa941b632e2e
Chris Rufus Sweeney <rufussweeney@gmail.com>
2025-01-09 17:25:27 -0700

    Fix root layout file

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,4 @@
-export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    );
-  }
-
-  export const metadata = {
-    title: 'Your App Name',
-    description: 'Your app description',
-  }
-  
-  export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    );
-  }
-
-  import './global.css'
+import './global.css'
 
 export const metadata = {
   title: 'Your App Name',
