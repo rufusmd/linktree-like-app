cf33f880f7849388aaf8d3de6b33ca48ca3e4744
Chris Rufus Sweeney <rufussweeney@gmail.com>
2025-01-11 11:20:18 -0700

    Consolidated dependencies and updated SendGrid integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "out", // Optional: Specifies the output directory for the build
+  output: "export", // Enables static HTML export
+  distDir: "out", // Specifies the output directory
   images: {
     unoptimized: true, // Disables Next.js image optimization
   },
   trailingSlash: true, // Ensures trailing slashes for routes
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL, // Environment variable for dynamic URL
-  },
-  experimental: {
-    serverActions: true, // Optional: if using server actions
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL, // Set dynamically
   },
 };
 
